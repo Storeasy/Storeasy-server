dadf52368b4e90a7041d0b2cbacb91500922640c
hyeinisfree <kimhyein7110@gmail.com>
2021-11-08 22:32:08 +0900

    add: app.module.ts에 JwtAuthGuard 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MorganInterceptor, MorganModule } from 'nest-morgan';
 import { ClassSerializerInterceptor } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import * as ormconfig from '../ormconfig';
+import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
 import { AuthController } from './auth/auth.controller';
 
 @Module({
@@ -25,6 +26,10 @@ import { AuthController } from './auth/auth.controller';
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
     },
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    }
   ],
 })
 export class AppModule {}
