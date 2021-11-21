84b8561b302bcb6b03d0facecb43c66ed51ea54f
hyeinisfree <kimhyein7110@gmail.com>
2021-11-22 00:56:22 +0900

    feat: 좋아요한 사용자 목록 조회, 좋아요한 페이지 목록 조회 controller 생성 #39

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Param, Post, Req } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, Post, Req, Res } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ResponseEntity } from 'src/config/res/response-entity';
 import { ResponseStatus } from 'src/config/res/response-status';
 import { LikeService } from './like.service';
@@ -25,5 +25,21 @@ export class LikeController {
   async likePage(@Req() req, @Param('pageId') pageId: number) {
     await this.likeService.likePage(req.user.userId, pageId);
     return ResponseEntity.OK(ResponseStatus.LIKE_PAGE_SUCCESS);
+  }
+
+  @ApiOperation({ summary: '좋아요한 사용자 목록 조회'})
+  @ApiOkResponse()
+  @Get('user')
+  async getLikeUsers(@Req() req) {
+    const data = await this.likeService.getLikeUsers(req.user.userId);
+    return ResponseEntity.OK_WITH(ResponseStatus.READ_ALL_LIKE_USERS_SUCCESS, data);
+  }
+
+  @ApiOperation({ summary: '좋아요한 페이지 목록 조회'})
+  @ApiOkResponse()
+  @Get('page')
+  async getLikePages(@Req() req) {
+    const data = await this.likeService.getLikePsges(req.user.userId);
+    return ResponseEntity.OK_WITH(ResponseStatus.READ_ALL_LIKE_PAGES_SUCCESS, data);
   }
 }
