19235d51ad0b8e8fc897d3c3ab07c2f281243188
hyeinisfree <kimhyein7110@gmail.com>
2021-11-24 04:23:54 +0900

    feat: ExplorePageResponseDto, ExploreUserResponseDto 생성 #46

--- src/explore/explore.module.ts
+++ src/explore/explore.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PageImageRepository } from 'src/repositories/page-image.repository';
+import { PageTagRepository } from 'src/repositories/page-tag.repository';
+import { PageRepository } from 'src/repositories/page.repository';
+import { ProfileTagRepository } from 'src/repositories/profile-tag.repository';
+import { ProfileRepository } from 'src/repositories/profile.repository';
 import { ExploreController } from './explore.controller';
 import { ExploreService } from './explore.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
-      
+      PageRepository,
+      ProfileRepository,
+      PageImageRepository,
+      PageTagRepository,
+      ProfileTagRepository,
     ]),
   ],
   controllers: [ExploreController],
--- src/explore/explore.service.ts
+++ src/explore/explore.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PageImageRepository } from 'src/repositories/page-image.repository';
+import { PageTagRepository } from 'src/repositories/page-tag.repository';
+import { PageRepository } from 'src/repositories/page.repository';
+import { ProfileTagRepository } from 'src/repositories/profile-tag.repository';
+import { ProfileRepository } from 'src/repositories/profile.repository';
 
 @Injectable()
 export class ExploreService {
-  constructor() {}
+  constructor(
+    private readonly pageRepository: PageRepository,
+    private readonly profileRepository: ProfileRepository,
+    private readonly pageImageRepository: PageImageRepository,
+    private readonly pageTagRepository: PageTagRepository,
+    private readonly profileTagRepository: ProfileTagRepository,
+  ) {}
 
   public async getRecommendPages(userId: number) {
     
