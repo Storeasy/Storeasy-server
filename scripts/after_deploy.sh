e5e0a9f1742b5443194767fe0591302fc3fe54b5
hyeinisfree <kimhyein7110@gmail.com>
2021-11-26 06:01:12 +0900

    chore: after_deploy.sh 수정

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -2,7 +2,4 @@
 REPOSITORY=/home/ubuntu/Storeasy-server
 cd $REPOSITORY
 
-sudo docker stop storeasy
-sudo docker rm storeasy
-
-sudo docker run -d -p 3000:3000 --name storeasy .
+sudo docker run -d -p 3000:3000 --name storeasy storeasy
